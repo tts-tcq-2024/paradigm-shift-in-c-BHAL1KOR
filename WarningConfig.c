16bd7ef3a85da218832dc30048b0ab9c37207179
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-04 17:12:43 +0530

    Create WarningConfig.c

---
+++ WarningConfig.c
@@ -0,0 +1,6 @@
+#include "WarningConfig.h"
+
+// Default configuration, can be edited for tuning
+// ENABLE (1) means warnings are enabled, DISABLE (0) means warnings are disabled
+
+WarningConfig warningConfig = {ENABLE, ENABLE, ENABLE};
